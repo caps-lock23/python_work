84d47ae9a29574fbaf8cc669ce3d940288859288
caps-lock23 <obnamiacada@gmail.com>
2025-01-18 09:34:34 +0800

    completed exercise 11_3

---
+++ 11_3/test_employee.py
@@ -0,0 +1,14 @@
+import pytest
+from employee import Employee
+
+@pytest.fixture
+def employee():
+    employee = Employee("Gerard", 'Cada', 1_000_000)
+    return employee
+
+def test_give_default_raise(employee):
+    employee.give_raise()
+    assert employee.annual_salary == 1_005_000
+
+def test_give_custom_raise(employee):
+    employee.give_raise(500_000)
